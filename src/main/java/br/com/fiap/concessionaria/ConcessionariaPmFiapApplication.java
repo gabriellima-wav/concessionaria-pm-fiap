3b2d412799f28a332fd949c417c0e4d54164ae5b
SudreErick <sudrerick@gmail.com>
2024-04-23 10:27:08 -0300

    Initial commit

---
+++ src/main/java/br/com/fiap/concessionaria/ConcessionariaPmFiapApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.concessionaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcessionariaPmFiapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcessionariaPmFiapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fiap/concessionaria/ConcessionariaPmFiapApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.concessionaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcessionariaPmFiapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
